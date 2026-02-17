8f17d9141e96afcf6d1c969f690459b5b7656e7c
Fejin Francis <fejinfm2000@gmail.com>
2026-02-17 17:15:07 +0530

    feat: add environment configuration files for development and production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,13 @@ export const environment = {
     token: '',
     owner: 'fejinfm2000',
     repo: 'bookora'
-  }
+  },
+  mega: {
+    email: '',
+    password: '',
+    apiKey: ''
+  },
+  adminEmails: [
+    'fejinfm2000@gmail.com'
+  ]
 };
